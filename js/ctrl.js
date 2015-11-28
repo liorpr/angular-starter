8e1e4c6a7abd0350622c2d4f1ebbb08b21640346
lior <lior@dauup.com>
2015-11-28 20:10:54 +0200

    first real commit

---
+++ js/ctrl.js
@@ -0,0 +1,16 @@
+
+( function( angular ) {
+
+    var starter = angular.module("starter");
+
+    starter.controller("starterCtrl", [ function() {
+        var ctrl = this;
+
+
+        return this;
+    }]);
+
+
+
+
+}( angular ));
